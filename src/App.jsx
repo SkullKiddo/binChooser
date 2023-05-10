142c63c94fb55ea3dd8ae65472bd071cac43f1ee
Gabriel Puigdemunt <gabriel.puigdemunt@bsc.es>
2023-05-10 19:01:27 +0200

    eliminada basura && varias fotos

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function ImageUploader() {
+  const [imageList, setImageList] = useState([]);
+
+
+  const handleFileChange = (e) => {
+    const files = e.target.files
+    const fileArray = [...files]
+    const readerArray = []
+    const imageArray = []
+    console.log("______ setting files",  fileArray)
+    fileArray.forEach(file =>{
+        console.log("______ file: ", file)
+        const reader = new FileReader()
+        readerArray.push(reader)
+        reader.onload = () => {
+          imageArray.push(reader.result)
+          if(imageArray.length ===  fileArray.length){
+            setImageList(imageArray)
+          }
+          // console.log("______ pushin to image array: ", reader.result)
+        }
+        reader.readAsDataURL(file);
+        console.log("______ equals: ", imageArray.length, " = ", fileArray.length)
+      }
+    )
+
+  };
+
+
+
+  function ImageList() {
+    console.log("______ printing files",  imageList)
+    return (
+      <div>
+        { imageList.map(image => (
+          <img src={image} width="500" height="500" alt=""/>
+        ))}
+      </div>
+    );
+  }
+  
+
+  return (
+    <div>
+    <input type="file" onChange={handleFileChange} multiple={true} />
+      <ImageList/>
+    </div>
+  );
+}
+
+export default ImageUploader;
