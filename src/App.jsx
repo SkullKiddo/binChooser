34fe9e9935b77ded51d9c01cab8168b83e689802
Gabriel Puigdemunt <gabriel.puigdemunt@bsc.es>
2023-05-11 12:45:35 +0200

    añadido drag&drop y mui packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function ImageUploader() {
   const [imageList, setImageList] = useState([]);
 
@@ -40,12 +45,12 @@ function ImageUploader() {
       </div>
     );
   }
-  
+
 
   return (
     <div>
-    <input type="file" onChange={handleFileChange} multiple={true} />
-      <ImageList/>
+    <input type="file" onChange={handleFileChange} onDrop={handleFileChange} multiple={true} />
+    <ImageList/>
     </div>
   );
 }
